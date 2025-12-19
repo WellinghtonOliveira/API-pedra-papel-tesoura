6e61850320577f5676ecf18a00d8f20316cbe3c7
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-12-19 09:24:44 -0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -1,47 +1,51 @@
 const API_URL = "http://localhost:3000"
-let userIdControl = null
+
+let matchId = null
+let playerId = crypto.randomUUID()
 
 document.getElementById("createMatch").addEventListener("click", async () => {
     const res = await fetch(`${API_URL}/match/create`, {
         method: "POST"
-    });
+    })
 
-    const data = await res.json();
-    userIdControl = data.controleId
+    const data = await res.json()
+    matchId = data.matchId
 
-    document.getElementById("matchId").innerText =
-        `Partida criada: ${userIdControl}`;
+    document.getElementById("matchId").innerText = `Partida: ${matchId}`
 
-    document.getElementById("play").style.display = "block";
-});
+    document.getElementById("play").style.display = "block"
+})
 
 async function play(choice) {
-
     await fetch(`${API_URL}/match/play`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            playerChoice: choice,
-            controleId: userIdControl
+            matchId,
+            playerId,
+            choice
         })
-    });
-
-    getResult();
-}
-
-async function getResult() {
-    const res = await fetch(`${API_URL}/match/result?matchId=${userIdControl}`, {
-        method: "POST"
     })
 
-    const data = await res.json();
+    pollResult()
+}
 
-    document.getElementById("result").innerHTML = `
-    <h2>Resultado</h2>
-    <p>Jogador 1: ${data.player1}</p>
-    <p>Jogador 2: ${data.player2}</p>
-    <strong>Vencedor: ${data.winner}</strong>
-  `;
+function pollResult() {
+    const interval = setInterval(async () => {
+        const res = await fetch(`${API_URL}/match/result?matchId=${matchId}`)
+        const data = await res.json()
+
+        if (data.status === "waiting") return
+
+        clearInterval(interval)
+
+        document.getElementById("result").innerHTML = `
+      <h2>Resultado</h2>
+      <p>Jogador 1: ${data.jogador1}</p>
+      <p>Jogador 2: ${data.jogador2}</p>
+      <strong>Vencedor: ${data.vencedor}</strong>
+    `
+    }, 2000)
 }
